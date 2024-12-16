e23eb4844fd8ffe4baa50029ad07dabbc2dfbf45
Dhruvil Nakrani <128470872+nd5a@users.noreply.github.com>
2024-12-16 18:14:43 +0530

    Enhanced Snake game

---
+++ Day21/inherit.py
@@ -0,0 +1,18 @@
+# inheritance
+
+class Vehicle:
+    def __init__(self):
+        self.wheels = 4
+    
+    def runc():
+        print("Vehicles can run")
+        
+class Car(Vehicle):
+    def __init__(self):
+        super().__init__()
+    
+    def stop():
+        print("Car Stoped")
+
+neno = Car()
+Car.runc()
